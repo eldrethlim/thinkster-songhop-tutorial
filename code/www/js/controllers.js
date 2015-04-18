1f63a8f9a930c2f32786ecf65c047668ca2cc78a
eldrethlim <eldrethlim@gmail.com>
2015-04-18 09:43:38 +0800

    Tutorial Complete!

--- code/www/js/controllers.js
+++ code/www/js/controllers.js
@@ -1,25 +1,127 @@
 angular.module('songhop.controllers', ['ionic', 'songhop.services'])
 
+/*
+Controller for the splash page
+*/
+.controller('SplashCtrl', function($scope, $state, User) {
+  // Signup/login via User.auth
+  $scope.submitForm = function(username, signingUp) {
+    User.auth(username, signingUp).then(function() { 
+      // Redirect to discover page after setting session
+      $state.go('tab.discover');
+
+    }, function() {
+      // Error handler
+      alert("Hmm, something went wrong! Try another username");
+
+    });
+  }
+})
 
 /*
 Controller for the discover page
 */
-.controller('DiscoverCtrl', function($scope) {
+.controller('DiscoverCtrl', function($scope, $timeout, $ionicLoading, User, Recommendations) {
+  // Helper functions for loading
+  var showLoading = function() {
+    $ionicLoading.show({
+      template: '<i class="ion-loading-c"></i>',
+      noBackdrop: true
+    });
+  }
 
+  var hideLoading = function() {
+    $ionicLoading.hide();
+  }
+
+  // Set loading to true the first time we retrieve songs from the server.
+  showLoading();
+
+  // Get out first songs
+  Recommendations.init()
+    .then(function(){
+      $scope.currentSong = Recommendations.queue[0];
+            
+      return Recommendations.playCurrentSong();
+    })
+    .then(function(){
+      hideLoading();
+      // turn loading off
+      $scope.currentSong.loaded = true;
+    });
+
+  // Cache image of the next album
+  $scope.nextAlbumImg = function() {
+    if (Recommendations.queue.length > 1) {
+      return Recommendations.queue[1].image_large;
+    }
+
+    return '';
+  }
+
+  // Skip or favourite songs
+  $scope.sendFeedback = function(bool) {
+
+    // Firstly, add to favourites if they favourited
+    if (bool) User.addSongToFavourites($scope.currentSong);
+
+    // Set variable for the correct animation sequence
+    $scope.currentSong.rated = bool;
+    $scope.currentSong.hide = true;
+
+    // Prepare next song
+    Recommendations.nextSong();
+
+    $timeout(function() {
+      // Timeout to allow animation to complete
+      $scope.currentSong = Recommendations.queue[0];
+      $scope.currentSong.loaded = false;
+    }, 250);
+
+    Recommendations.playCurrentSong().then(function() {
+      $scope.currentSong.loaded = true;
+    });
+  }
 })
-
 
 /*
 Controller for the favorites page
 */
-.controller('FavoritesCtrl', function($scope) {
+.controller('FavoritesCtrl', function($scope, User, $window) {
 
+  $scope.username = User.username;
+
+  $scope.openSong = function(song) {
+    $window.open(song.open_url, "_system");
+  }
+
+  $scope.favourites = User.favourites;
+
+  $scope.removeSong = function(song, index) {
+    User.removeSongFromFavourites(song, index);
+  };
 })
-
 
 /*
 Controller for our tab bar
 */
-.controller('TabsCtrl', function($scope) {
+.controller('TabsCtrl', function($scope, Recommendations, User) {
+  $scope.favCount = User.favouriteCount;
 
+  $scope.enteringFavourites = function() {
+    // Reset new favourites to 0 when we enter favourites tab.
+    User.newFavourites = 0;
+    Recommendations.haltAudio();
+  }
+
+  $scope.leavingFavourites = function() {
+    Recommendations.init();
+  }
+
+  $scope.logout = function() {
+    User.destroySession();
+
+    // Redirection is used to remove cached items
+    $window.location.href = 'index.html';
+  }
 });
--- code/www/js/services.js
+++ code/www/js/services.js
@@ -1,1 +1,177 @@
-angular.module('songhop.services', []);
+angular.module('songhop.services', ['ionic.utils'])
+
+.factory('Recommendations', function($http, $q, SERVER) {
+  var media;
+  var o = {
+    queue: []
+  };
+
+  o.init = function() {
+    if (o.queue.length === 0) {
+      // if there's nothing in queue, fill it
+      return o.getNextSongs();
+    } else {
+      return o.playCurrentSong();
+    }
+  }
+
+  o.getNextSongs = function() {
+    return $http({
+      method: 'GET',
+      url: SERVER.url + '/recommendations'
+    }).success(function(data) {
+      o.queue = o.queue.concat(data);
+    });
+  }
+
+  o.nextSong = function() {
+    // pop the index 0 off
+    o.queue.shift();
+
+    // end song
+    o.haltAudio();
+
+    // refills array if song queue is low
+    if (o.queue.length <= 3) {
+      o.getNextSongs();
+    }
+  }
+
+  o.playCurrentSong = function() {
+    var defer = $q.defer();
+
+    // play the current song's preview
+    media = new Audio(o.queue[0].preview_url);
+
+    // when song is loaded, resolve the promise to let the controller know.
+    media.addEventListener("loadeddata", function() {
+      defer.resolve();
+    });
+
+    media.play();
+
+    return defer.promise;
+  }
+
+  o.haltAudio = function() {
+    if (media) media.pause();
+  }
+
+  return o;
+})
+
+.factory('User', function($http, SERVER, $q, $localstorage) {
+
+  var o = {
+    username: false,
+    session_id: false,
+    favourites: [],
+    newFavourites: 0 
+  }
+
+  // Authentication
+  o.auth = function(username, signingUp) {
+    var authRoute;
+
+    if (signingUp) {
+      authRoute = 'signup';
+    } else {
+      authRoute = 'login';
+    }
+
+  return $http.post(SERVER.url + '/' + authRoute, { username: username }).
+    success(function(data) {
+      debugger
+      o.setSession(data.username, data.session_id, data.favourites);
+    });
+  }
+
+  // Set session data
+  o.setSession = function(username, session_id, favourites) {
+    if (username) o.username = username;
+    if (session_id) o.session_id = session_id;
+    if (favourites) o.favourites = favourites;
+
+    // Set data in
+    $localstorage.setObject('user', { username: username, session_id: session_id });
+  }
+  // Check session data
+  o.checkSession = function() {
+    var defer = $q.defer();
+
+    if (o.session_id) {
+      // If this session is already initialized in the service
+      defer.resolve(true);
+    } else {
+      // Detect if there's a session in localstorage from previous use.
+      // If there is one, pull into our service
+      var user = $localstorage.getObject('user');
+
+      if (user.username) {
+        // If there's a user, lets grab their favourites from the server
+        o.setSession(user.username, user.session_id);
+        o.populateFavourites().then(function() {
+          defer.resolve(true);
+        });
+      } else {
+        // No user info in localstorage, reject
+        defer.resolve(false);
+      }
+    }
+
+    return defer.promise;
+  }
+
+  // Wipe out session data
+  o.destroySession = function() {
+    $localstorage.setObject('user', {});
+    o.username = false;
+    o.session_id = false;
+    o.favourites = [];
+    o.newFavourites = 0;
+  }
+
+  o.addSongToFavourites = function(song) {
+    // make sure there's a song to add
+    if (!song) return false;
+
+    // add to favourites array
+    o.favourites.unshift(song);
+    o.newFavourites++;
+
+    // persist song favouriting to server
+    return $http.post(SERVER.url + '/favorites', { session_id: o.session_id, song_id: song.song_id });
+  }
+
+  o.removeSongFromFavourites = function(song,index) {
+    // make sure there's a song to remove
+    if (!song) return false;
+
+    // remove from favourites array
+    o.favourites.splice(index, 1);
+
+    // persist song unfavouriting to server
+    return $http({
+      method: 'DELETE',
+      url: SERVER.url + '/favorites',
+      params: { session_id: o.session_id, song_id: song.song_id }
+    });
+  }
+
+  o.populateFavourites = function() {
+    return $http({
+      method: 'GET',
+      url: SERVER.url + '/favorites',
+      params: { session_id: o.session_id }
+    }).success(function(data) {
+      // merge data into the queue
+      o.favourites = data;
+    });
+  }
+
+  o.favouriteCount = function() {
+    return o.newFavourites;
+  }
+
+  return o;
+});
